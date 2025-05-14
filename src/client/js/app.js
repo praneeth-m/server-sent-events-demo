23fed3a28fa3ea18466dc47aebdfbb31fcb7e814
Praneeth <praneeth.m@gmail.com>
2025-05-14 12:05:36 -0500

    init commit

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -2,74 +2,119 @@
 
 const app = {
   updateEventsReceived: (event) => {
-    document.getElementById('eventLog').insertAdjacentHTML('afterbegin', `<br>${event.lastEventId}: (${event.type}) ${event.data}`);
+    document
+      .getElementById('eventLog')
+      .insertAdjacentHTML(
+        'beforeend',
+        `<br><br>id: ${event.lastEventId}<br>event: ${event.type}<br>data: ${event.data}`
+      )
   },
 
   resetStartButton: () => {
-    document.getElementById('startEvents').value = 'Start Events';
+    document.getElementById('startEvents').value = 'Start Events'
   },
 
   startEvents: () => {
-    app.eventSource = new EventSource('http://localhost:5000/randomNamedEvents');
+    app.eventSource = new EventSource('http://localhost:5001/interactionApi')
 
-    app.eventSource.onmessage = (e) => {
-      console.log(e);
-      if (e.lastEventId === '-1') {
-        app.eventSource.close();
-        document.getElementById('eventLog').insertAdjacentHTML('afterbegin', '<br>End of event stream from server.');
-        app.resetStartButton();
-      }
-    };
+    // app.eventSource.onmessage = (e) => {
+    //   console.log(e)
+    //   if (e.lastEventId === '-1') {
+    //     app.eventSource.close()
+    //     document
+    //       .getElementById('eventLog')
+    //       .insertAdjacentHTML(
+    //         'beforeend',
+    //         '<br><br>End of event stream from server.'
+    //       )
+    //     app.resetStartButton()
+    //   }
+    // }
 
-    app.eventSource.addEventListener('coinToss', (e) => {
-      console.log(e);
-      document.getElementById('coinToss').innerHTML = `${e.data}`;
-      app.updateEventsReceived(e);
-    });
+    // app.eventSource.addEventListener('coinToss', (e) => {
+    //   console.log(e)
+    //   document.getElementById('coinToss').innerHTML = `${e.data}`
+    //   app.updateEventsReceived(e)
+    // })
 
-    app.eventSource.addEventListener('dieRoll', (e) => {
-      console.log(e);
-      document.getElementById('dieRoll').innerHTML = `${e.data}`;
-      app.updateEventsReceived(e);
-    });
+    // app.eventSource.addEventListener('dieRoll', (e) => {
+    //   console.log(e)
+    //   document.getElementById('dieRoll').innerHTML = `${e.data}`
+    //   app.updateEventsReceived(e)
+    // })
 
-    app.eventSource.addEventListener('catFact', (e) => {
-      console.log(e);
-      document.getElementById('catFact').innerHTML = `${e.data}`;
-      app.updateEventsReceived(e);
-    });
+    // app.eventSource.addEventListener('catFact', (e) => {
+    //   console.log(e)
+    //   document.getElementById('catFact').innerHTML = `${e.data}`
+    //   app.updateEventsReceived(e)
+    // })
 
-    app.eventSource.addEventListener('meme', (e) => {
-      console.log(e);
-      document.getElementById('meme').innerHTML = `<img class="memeImage" src="${e.data}">`;
-      app.updateEventsReceived(e);
-    });
+    // app.eventSource.addEventListener('meme', (e) => {
+    //   console.log(e)
+    //   document.getElementById(
+    //     'meme'
+    //   ).innerHTML = `<img class="memeImage" src="${e.data}">`
+    //   app.updateEventsReceived(e)
+    // })
+
+    app.eventSource.addEventListener('progress', (e) => {
+      console.log(e)
+      // document.getElementById(
+      //   'meme'
+      // ).innerHTML = `<img class="memeImage" src="${e.data}">`
+      app.updateEventsReceived(e)
+    })
+
+    app.eventSource.addEventListener('response', (e) => {
+      console.log(e)
+      // document.getElementById(
+      //   'meme'
+      // ).innerHTML = `<img class="memeImage" src="${e.data}">`
+      app.updateEventsReceived(e)
+      app.eventSource.close()
+      document
+        .getElementById('eventLog')
+        .insertAdjacentHTML(
+          'beforeend',
+          '<br><br>End of event stream from server.'
+        )
+      app.resetStartButton()
+    })
 
     app.eventSource.addEventListener('error', (e) => {
-      console.log('got an error');
-      console.log(e);
-      app.resetStartButton();
-    });
-  }
-};
+      console.log('got an error')
+      console.log(e)
+      app.resetStartButton()
+    })
+  },
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-  const browserSupportElem = document.getElementById('browserSupport');
+  const browserSupportElem = document.getElementById('browserSupport')
 
-  if (!! window.EventSource) {
-    browserSupportElem.innerHTML = 'This browser supports <tt>EventSource</tt>! <input type="submit" class="startEvents" id="startEvents" value="Start Events">';
-    document.getElementById('startEvents').addEventListener('click', function(event) {
-      if (this.value === 'Start Events') {
-        this.value = 'Stop Events';
-        app.startEvents();
-      } else {
-        app.eventSource.close();
-        document.getElementById('eventLog').insertAdjacentHTML('afterbegin', '<br>Event stream closed by browser.');
-        app.resetStartButton();
-      }
-    });
+  if (!!window.EventSource) {
+    browserSupportElem.innerHTML =
+      'This browser supports <tt>EventSource</tt>! <input type="submit" class="startEvents" id="startEvents" value="Start Events">'
+    document
+      .getElementById('startEvents')
+      .addEventListener('click', function (event) {
+        if (this.value === 'Start Events') {
+          this.value = 'Stop Events'
+          app.startEvents()
+        } else {
+          app.eventSource.close()
+          document
+            .getElementById('eventLog')
+            .insertAdjacentHTML(
+              'beforeend',
+              '<br><br>Event stream closed by browser.'
+            )
+          app.resetStartButton()
+        }
+      })
   } else {
-    browserSupportElem.innerHTML = 'This browser does not support <tt>EventSource</tt> :(';
-    browserSupportElem.classList.add('unsupportedBrowser');
+    browserSupportElem.innerHTML =
+      'This browser does not support <tt>EventSource</tt> :('
+    browserSupportElem.classList.add('unsupportedBrowser')
   }
-});
+})
--- src/server/server.js
+++ src/server/server.js
@@ -1,130 +1,241 @@
 // server/server.js
 
-const http = require('http');
-const PORT = process.env.PORT || 5000;
+const http = require('http')
+const PORT = process.env.PORT || 5001
 
-const catFacts = [
-  'On average, cats spend 2/3 of every day sleeping.',
-  'Unlike dogs, cats do not have a sweet tooth.',
-  'When a cat chases its prey, it keeps its head level.',
-  'The technical term for a cat\'s hairball is a bezoar.',
-  'A group of cats is called a clowder.',
-  'Cats make about 100 different sounds.',
-  'There are over 500 million domestic cats globally.',
-  'Cats are the most popular pet in North America.',
-  'A cat\'s hearing is better than a dog\'s.',
-  'Researchers are unsure exactly how a cat purrs.',
-  'The biggest wildcat today is the Siberian Tiger.',
-  'The smallest wildcat today is the Black-footed cat.',
-  'A cat\'s sight is both better and worse than humans.',
-  'Cats have 32 muscles that control the outer ear.'
-];
+// const catFacts = [
+//   'On average, cats spend 2/3 of every day sleeping.',
+//   'Unlike dogs, cats do not have a sweet tooth.',
+//   'When a cat chases its prey, it keeps its head level.',
+//   "The technical term for a cat's hairball is a bezoar.",
+//   'A group of cats is called a clowder.',
+//   'Cats make about 100 different sounds.',
+//   'There are over 500 million domestic cats globally.',
+//   'Cats are the most popular pet in North America.',
+//   "A cat's hearing is better than a dog's.",
+//   'Researchers are unsure exactly how a cat purrs.',
+//   'The biggest wildcat today is the Siberian Tiger.',
+//   'The smallest wildcat today is the Black-footed cat.',
+//   "A cat's sight is both better and worse than humans.",
+//   'Cats have 32 muscles that control the outer ear.',
+// ]
 
-const memes = [
-  'https://imgflip.com/s/meme/Creepy-Condescending-Wonka.jpg',
-  'https://imgflip.com/s/meme/Doge.jpg',
-  'https://imgflip.com/s/meme/Philosoraptor.jpg',
-  'https://imgflip.com/s/meme/Conspiracy-Keanu.jpg',
-  'https://imgflip.com/s/meme/Table-Flip-Guy.jpg'
-];
+const interactionApiResponseMessages = [
+  {
+    type: 'progress',
+    progress: {
+      // type: 'status',
+      content:
+        'Hello, im an agent and I am going to choose a tool for your request!',
+    },
+    response: null,
+  },
+  {
+    type: 'progress',
+    progress: {
+      // type: 'status',
+      content: 'calling tool <adder>',
+    },
+    response: null,
+  },
+  {
+    type: 'progress',
+    progress: {
+      // type: 'status',
+      content: 'adding these two numbers is pretty tough',
+    },
+    response: null,
+  },
+  {
+    type: 'progress',
+    progress: {
+      // type: 'status',
+      content: 'tool call <adder> completed',
+    },
+    response: null,
+  },
+  {
+    type: 'progress',
+    progress: {
+      // type: 'status',
+      content: 'Bye bye!',
+    },
+    response: null,
+  },
+  {
+    type: 'response',
+    progress: null,
+    response: {
+      agentId: '12345',
+      messages: [
+        {
+          id: 'xyz',
+          content: {
+            type: 'text',
+            text: '1 + 2 = 3',
+          },
+          actionId: 'abc_321',
+          role: 'action',
+        },
+      ],
+    },
+  },
+]
 
-function getRandomIndex(low, high) {
-  const min = Math.ceil(low);
-  const max = Math.floor(high);
+// const memes = [
+//   'https://imgflip.com/s/meme/Creepy-Condescending-Wonka.jpg',
+//   'https://imgflip.com/s/meme/Doge.jpg',
+//   'https://imgflip.com/s/meme/Philosoraptor.jpg',
+//   'https://imgflip.com/s/meme/Conspiracy-Keanu.jpg',
+//   'https://imgflip.com/s/meme/Table-Flip-Guy.jpg',
+// ]
 
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// function getRandomIndex(low, high) {
+//   const min = Math.ceil(low)
+//   const max = Math.floor(high)
 
-function createRandomCatFactMessage() {
-  console.log('Sending cat fact message.');
-  return (`event: catFact\ndata:${catFacts[getRandomIndex(0, catFacts.length - 1)]}\n\n\n`);
-}
+//   return Math.floor(Math.random() * (max - min + 1)) + min
+// }
 
-function createCoinTossMessage() {
-  console.log('Sending coin toss message.');
-  return (`event: coinToss\ndata:${getRandomIndex(0, 1) ? 'Heads' : 'Tails'}\n\n\n`);
-}
+// function createRandomCatFactMessage() {
+//   console.log('Sending cat fact message.')
+//   return `event: catFact\ndata:${
+//     catFacts[getRandomIndex(0, catFacts.length - 1)]
+//   }\n\n\n`
+// }
 
-function createDieRollMessage() {
-  console.log('Sending die roll message.');
-  return (`event: dieRoll\ndata:${getRandomIndex(1, 6)}\n\n\n`);
-}
+// function createCoinTossMessage() {
+//   console.log('Sending coin toss message.')
+//   return `event: coinToss\ndata:${
+//     getRandomIndex(0, 1) ? 'Heads' : 'Tails'
+//   }\n\n\n`
+// }
 
-function createRandomMemeMessage() {
-  console.log('Sending meme message.');
-  return (`event: meme\ndata:${memes[getRandomIndex(0, memes.length - 1)]}\n\n\n`);
-}
+// function createDieRollMessage() {
+//   console.log('Sending die roll message.')
+//   return `event: dieRoll\ndata:${getRandomIndex(1, 6)}\n\n\n`
+// }
 
-function createRandomNamedEvents(request, response) {
+// function createRandomMemeMessage() {
+//   console.log('Sending meme message.')
+//   return `event: meme\ndata:${memes[getRandomIndex(0, memes.length - 1)]}\n\n\n`
+// }
+
+// function createRandomNamedEvents(request, response) {
+//   response.writeHead(200, {
+//     Connection: 'keep-alive',
+//     'Content-Type': 'text/event-stream',
+//     'Cache-Control': 'no-cache',
+//   })
+
+//   let eventsSent = 1
+
+//   const interval = setInterval(() => {
+//     if (eventsSent === 31) {
+//       clearInterval(interval)
+//       console.log('Sent 30 events, stopping.')
+//       response.write('id: -1\ndata:\n\n\n')
+//       response.end()
+//       return
+//     }
+
+//     response.write(`id: ${eventsSent < 10 ? '0' : ''}${eventsSent}\n`)
+
+//     const msgType = getRandomIndex(0, 3)
+
+//     switch (msgType) {
+//       case 0:
+//         response.write(createCoinTossMessage())
+//         eventsSent += 1
+//         break
+//       case 1:
+//         response.write(createDieRollMessage())
+//         eventsSent += 1
+//         break
+//       case 2:
+//         response.write(createRandomCatFactMessage())
+//         eventsSent += 1
+//         break
+//       case 3:
+//         response.write(createRandomMemeMessage())
+//         eventsSent += 1
+//         break
+//     }
+//   }, 3000)
+
+//   request.on('close', () => {
+//     clearInterval(interval)
+//     response.end()
+//     console.log('Stopped sending events as client closed the connection.')
+//   })
+// }
+
+function createInteractipApiEvents(request, response) {
   response.writeHead(200, {
     Connection: 'keep-alive',
     'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache'
-  });
+    'Cache-Control': 'no-cache',
+  })
 
-  let eventsSent = 1;
+  let eventsSent = 0
 
   const interval = setInterval(() => {
-    if (eventsSent === 31) {
-      clearInterval(interval);
-      console.log('Sent 30 events, stopping.');
-      response.write('id: -1\ndata:\n\n\n');
-      response.end();
-      return;
+    if (eventsSent >= interactionApiResponseMessages.length) {
+      clearInterval(interval)
+      console.log('Sent all events, stopping.')
+      response.write('id: -1\ndata:\n\n\n')
+      response.end()
+      return
     }
 
-    response.write(`id: ${eventsSent < 10 ? '0' : ''}${eventsSent}\n`);
+    response.write(`id: ${eventsSent}\n`)
 
-    const msgType = getRandomIndex(0, 3);
-
-    switch (msgType) {
-      case 0:
-        response.write(createCoinTossMessage());
-        eventsSent += 1;
-        break;
-      case 1:
-        response.write(createDieRollMessage());
-        eventsSent += 1;
-        break;
-      case 2:
-        response.write(createRandomCatFactMessage()); 
-        eventsSent += 1;   
-        break;
-      case 3:
-        response.write(createRandomMemeMessage());
-        eventsSent += 1;
-        break;
+    const message = interactionApiResponseMessages[eventsSent]
+    if (message.type === 'progress') {
+      response.write(
+        `event: progress\ndata:${JSON.stringify(message.progress)}\n\n\n`
+      )
+    } else {
+      response.write(
+        `event: response\ndata:${JSON.stringify(message.response)}\n\n\n`
+      )
     }
-  }, 3000);
+    eventsSent += 1
+  }, 1000)
 
   request.on('close', () => {
-    clearInterval(interval);
-    response.end();
-    console.log('Stopped sending events as client closed the connection.');
-  });
+    clearInterval(interval)
+    response.end()
+    console.log('Stopped sending events as client closed the connection.')
+  })
 }
 
-http.createServer((request, response) => {
-  response.setHeader('Access-Control-Allow-Origin', '*');
-  response.setHeader('Access-Control-Request-Method', '*');
-  response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-  response.setHeader('Access-Control-Allow-Headers', '*');
+http
+  .createServer((request, response) => {
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.setHeader('Access-Control-Request-Method', '*')
+    response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET')
+    response.setHeader('Access-Control-Allow-Headers', '*')
 
-  if (request.method === 'OPTIONS') {
-    response.writeHead(200);
-    response.end();
-    return;
-  }
+    if (request.method === 'OPTIONS') {
+      response.writeHead(200)
+      response.end()
+      return
+    }
 
-  switch (request.url) {
-    case '/randomNamedEvents':
-      createRandomNamedEvents(request, response);
-      break;
-    default:
-      // Unknown URL
-      response.writeHead(404);
-      response.end();
-  }
-}).listen(PORT);
+    switch (request.url) {
+      // case '/randomNamedEvents':
+      //   createRandomNamedEvents(request, response)
+      //   break
+      case '/interactionApi':
+        createInteractipApiEvents(request, response)
+        break
+      default:
+        // Unknown URL
+        response.writeHead(404)
+        response.end()
+    }
+  })
+  .listen(PORT)
 
-console.log(`server-sent-events-demo server running on port ${PORT}`);
+console.log(`server-sent-events-demo server running on port ${PORT}`)
